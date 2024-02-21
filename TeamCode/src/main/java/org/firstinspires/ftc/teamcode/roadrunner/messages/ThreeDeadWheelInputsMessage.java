2dbcff28333c329d405d9e00f8bad98699b1b840
ProNoob1029 <85897880+ProNoob1029@users.noreply.github.com>
2024-02-22 00:09:23 +0200

    fixed merge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/messages/ThreeDeadWheelInputsMessage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/messages/ThreeDeadWheelInputsMessage.java
@@ -2,12 +2,8 @@ package org.firstinspires.ftc.teamcode.roadrunner.messages;
 
 import com.acmerobotics.roadrunner.ftc.PositionVelocityPair;
 
-<<<<<<<< HEAD:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/messages/ThreeDeadWheelEncodersMessage.java
 /** @noinspection unused*/
-public final class ThreeDeadWheelEncodersMessage {
-========
 public final class ThreeDeadWheelInputsMessage {
->>>>>>>> remote/master:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/messages/ThreeDeadWheelInputsMessage.java
     public long timestamp;
     public PositionVelocityPair par0;
     public PositionVelocityPair par1;
